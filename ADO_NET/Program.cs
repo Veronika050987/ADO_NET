27f58db560507c998c97e8b569a7e7fa93ececcd
Veronika050987 <veronicaviv@mail.ru>
2025-09-13 17:17:45 +0300

    Add project files.

---
+++ ADO_NET/Program.cs
@@ -0,0 +1,19 @@
+﻿//ADO_NET
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//ADO.NET:
+using System.Data.SqlClient;
+
+namespace ADO_NET
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello ADO.NET");
+		}
+	}
+}
