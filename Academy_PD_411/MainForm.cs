c0e9ea00f7df515d2649be8f7f601e82dc37835a
Veronika050987 <veronicaviv@mail.ru>
2025-09-24 23:19:38 +0300

    rebuild DONE

--- Academy_PD_411/MainForm.cs
+++ Academy_PD_411/MainForm.cs
@@ -18,7 +18,24 @@ namespace Academy_PD_411
 		SqlConnection connection;
 		Dictionary<string, int> d_groupDirection;
 
-
+		Query[] queries = new Query[]
+		{
+			new Query
+			(
+"stud_id,FORMATMESSAGE(N'%s %s %s',last_name,first_name,middle_name) AS N'Student', group_name AS N'Group',direction_name AS N'Direction'",
+"Students,Groups,Directions",
+"[group]=group_id AND direction=direction_id"
+			),
+			new Query
+			(
+				"group_id,group_name,direction_name",
+				"Groups,Directions",
+				"direction=direction_id"
+			),
+			new Query("*", "Directions"),
+			new Query("*", "Disciplines"),
+			new Query("*", "Teachers")
+		};
 
 		readonly string[] statusBarMessages = new string[]
 		{
@@ -43,14 +60,20 @@ namespace Academy_PD_411
 			comboBoxGroupsDirection.SelectedIndex = 0;
 
 			tabControl.SelectedIndex = 1;
+
+			for(int i = 0;i < tabControl.TabCount;i++)
+			{
+				(this.Controls.Find($"dataGridView{tabControl.TabPages[i].Name.Remove(0, "tabPage".Length)}", true) [0] as DataGridView).RowsAdded 
+					+= new DataGridViewRowsAddedEventHandler(this.dataGridViewChanged);
+			}
 		}
 		
 		void LoadTab(int i)
 		{
 			string tableName = tabControl.TabPages[i].Name.Remove(0, "tabPage".Length);
 			DataGridView dataGridView = this.Controls.Find($"dataGridView{tableName}", true)[0] as DataGridView;
-			dataGridView.DataSource = Select("*", tableName);
-			toolStripStatusLabel.Text = $"{statusBarMessages[i]}: {dataGridView.RowCount - 1}";
+			dataGridView.DataSource = Select(queries[i].Fields, queries[i].Tables, queries[i].Condition);
+			//toolStripStatusLabel.Text = $"{statusBarMessages[i]}: {dataGridView.RowCount - 1}";
 		}
 		void FillStatusBar(int i)
 		{
@@ -117,6 +140,10 @@ namespace Academy_PD_411
 		private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
 		{
 			LoadTab((sender as TabControl).SelectedIndex);
+		}
+		private void dataGridViewChanged(object sender, EventArgs e)
+		{
+			toolStripStatusLabel.Text = $"{statusBarMessages[tabControl.SelectedIndex]}: {(sender as DataGridView).RowCount - 1}";
 		}
 	}
 }
