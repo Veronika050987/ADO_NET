3f52a1bedc5c764040da09f661856555c4763b68
Veronika050987 <veronicaviv@mail.ru>
2025-10-01 19:59:04 +0300

    comboboxes from dataset done

--- Data_Set/MainForm.Designer.cs
+++ Data_Set/MainForm.Designer.cs
@@ -28,13 +28,46 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.comboBoxStudentsGroup = new System.Windows.Forms.ComboBox();
+			this.comboBoxStudentsDirection = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// comboBoxStudentsGroup
+			// 
+			this.comboBoxStudentsGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxStudentsGroup.FormattingEnabled = true;
+			this.comboBoxStudentsGroup.Location = new System.Drawing.Point(39, 42);
+			this.comboBoxStudentsGroup.Name = "comboBoxStudentsGroup";
+			this.comboBoxStudentsGroup.Size = new System.Drawing.Size(235, 24);
+			this.comboBoxStudentsGroup.TabIndex = 0;
+			// 
+			// comboBoxStudentsDirection
+			// 
+			this.comboBoxStudentsDirection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxStudentsDirection.FormattingEnabled = true;
+			this.comboBoxStudentsDirection.Location = new System.Drawing.Point(336, 42);
+			this.comboBoxStudentsDirection.Name = "comboBoxStudentsDirection";
+			this.comboBoxStudentsDirection.Size = new System.Drawing.Size(238, 24);
+			this.comboBoxStudentsDirection.TabIndex = 1;
+			this.comboBoxStudentsDirection.SelectedIndexChanged += new System.EventHandler(this.comboBoxStudentsDirection_SelectedIndexChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.comboBoxStudentsDirection);
+			this.Controls.Add(this.comboBoxStudentsGroup);
+			this.Name = "MainForm";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ComboBox comboBoxStudentsGroup;
+		private System.Windows.Forms.ComboBox comboBoxStudentsDirection;
 	}
 }
 
