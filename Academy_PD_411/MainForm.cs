8eda92bd12b57d20a1beca0b5a4780f9bdd7cb51
Veronika050987 <veronicaviv@mail.ru>
2025-10-05 17:56:50 +0300

    adding student almost done

--- Academy_PD_411/MainForm.cs
+++ Academy_PD_411/MainForm.cs
@@ -266,7 +266,7 @@ namespace Academy_PD_411
 					(
 						"Students",
 						"last_name, first_name, middle_name, birth_date, email, phone, [group]",
-						$"N'{student.Student.LastName}', N'{student.Student.FirstName}', N'{student.Student.MiddleName}', N'{student.Student.BirthDate}', N'{student.Student.Email}', N'{student.Student.Phone}', N'{student.Student.Group}'",
+						$"N'{student.Student.LastName}', N'{student.Student.FirstName}', N'{student.Student.MiddleName}', {student.Student.BirthDate}, N'{student.Student.Email}', N'{student.Student.Phone}', N'{student.Student.Group}'",
 						student.Student.Photo
 					);
 
