bec460f9bdac3df8fd11ea70c10cb8126c497eb8
Veronika050987 <veronicaviv@mail.ru>
2025-10-06 12:48:17 +0300

    add photo student DONE

--- Academy_PD_411/MainForm.cs
+++ Academy_PD_411/MainForm.cs
@@ -150,23 +150,6 @@ namespace Academy_PD_411
 
 			SqlCommand command = new SqlCommand(cmd, connection);
 
-			string escapedValues = values.Split(',')
-				.Select(value =>
-				{
-					value = value.Trim();
-					if (string.IsNullOrEmpty(value) || value.ToLower() == "null")
-					{
-						return "NULL";
-					}
-					else
-					{
-						return "'" + value.Replace("'", "''") + "'";
-					}
-				})
-				.Aggregate((a, b) => a + ", " + b);
-
-			command.CommandText = $"INSERT INTO {table} ({fields}) VALUES ({escapedValues})";
-
 			if (photo != null && photo.Length > 0)
 			{
 				SqlParameter photoParam = new SqlParameter("@Photo", SqlDbType.VarBinary, photo.Length);
@@ -297,15 +280,13 @@ namespace Academy_PD_411
 			{
 				try
 				{
-					string birthDateFormatted = student.Student.BirthDate;
-
-					string values = $"N'{student.Student.LastName}', N'{student.Student.FirstName}', N'{student.Student.MiddleName}', '{birthDateFormatted}', N'{student.Student.Email}', N'{student.Student.Phone}', N'{student.Student.Group}'";
+					string values = $"N'{student.Student.LastName}', N'{student.Student.FirstName}', N'{student.Student.MiddleName}', N'{student.Student.BirthDate}', N'{student.Student.Email}', N'{student.Student.Phone}', N'{student.Student.Group}'";
 
 					Insert
 					(
 						"Students",
 						"last_name, first_name, middle_name, birth_date, email, phone, [group]",
-						$"N'{student.Student.LastName}', N'{student.Student.FirstName}', N'{student.Student.MiddleName}', '{birthDateFormatted}', N'{student.Student.Email}', N'{student.Student.Phone}', N'{student.Student.Group}'",
+						$"N'{student.Student.LastName}', N'{student.Student.FirstName}', N'{student.Student.MiddleName}', N'{student.Student.BirthDate}', N'{student.Student.Email}', N'{student.Student.Phone}', N'{student.Student.Group}'",
 						student.Student.Photo
 					);
 
