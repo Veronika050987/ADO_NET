16e398dcf24c7b75e12c3ab4712c5aa51729ee56
Veronika050987 <veronicaviv@mail.ru>
2025-10-11 13:21:31 +0300

    Human form start

--- Academy_PD_411/StudentForm.Designer.cs
+++ Academy_PD_411/StudentForm.Designer.cs
@@ -34,6 +34,20 @@
 			this.buttonOK = new System.Windows.Forms.Button();
 			this.buttonCancel = new System.Windows.Forms.Button();
 			this.labelID = new System.Windows.Forms.Label();
+			this.pictureBoxPhoto = new System.Windows.Forms.PictureBox();
+			this.textBoxPhone = new System.Windows.Forms.TextBox();
+			this.textBoxEmail = new System.Windows.Forms.TextBox();
+			this.dateTimePickerBirthDate = new System.Windows.Forms.DateTimePicker();
+			this.textBoxMiddleName = new System.Windows.Forms.TextBox();
+			this.textBoxFirstName = new System.Windows.Forms.TextBox();
+			this.textBoxLastName = new System.Windows.Forms.TextBox();
+			this.labelPhone = new System.Windows.Forms.Label();
+			this.labelEmail = new System.Windows.Forms.Label();
+			this.labelBirthDate = new System.Windows.Forms.Label();
+			this.labelMiddleName = new System.Windows.Forms.Label();
+			this.labelFirstName = new System.Windows.Forms.Label();
+			this.labelLastName = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxPhoto)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// labelGroup
@@ -49,7 +63,7 @@
 			// 
 			this.comboBoxGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.comboBoxGroup.FormattingEnabled = true;
-			this.comboBoxGroup.Location = new System.Drawing.Point(170, 281);
+			this.comboBoxGroup.Location = new System.Drawing.Point(177, 281);
 			this.comboBoxGroup.Name = "comboBoxGroup";
 			this.comboBoxGroup.Size = new System.Drawing.Size(216, 24);
 			this.comboBoxGroup.TabIndex = 13;
@@ -99,11 +113,131 @@
 			this.labelID.Text = "ID";
 			this.labelID.Visible = false;
 			// 
+			// pictureBoxPhoto
+			// 
+			this.pictureBoxPhoto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBoxPhoto.Location = new System.Drawing.Point(437, 26);
+			this.pictureBoxPhoto.Name = "pictureBoxPhoto";
+			this.pictureBoxPhoto.Size = new System.Drawing.Size(245, 285);
+			this.pictureBoxPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBoxPhoto.TabIndex = 42;
+			this.pictureBoxPhoto.TabStop = false;
+			// 
+			// textBoxPhone
+			// 
+			this.textBoxPhone.Location = new System.Drawing.Point(177, 245);
+			this.textBoxPhone.Name = "textBoxPhone";
+			this.textBoxPhone.Size = new System.Drawing.Size(216, 22);
+			this.textBoxPhone.TabIndex = 41;
+			// 
+			// textBoxEmail
+			// 
+			this.textBoxEmail.Location = new System.Drawing.Point(177, 201);
+			this.textBoxEmail.Name = "textBoxEmail";
+			this.textBoxEmail.Size = new System.Drawing.Size(216, 22);
+			this.textBoxEmail.TabIndex = 40;
+			// 
+			// dateTimePickerBirthDate
+			// 
+			this.dateTimePickerBirthDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dateTimePickerBirthDate.Location = new System.Drawing.Point(177, 157);
+			this.dateTimePickerBirthDate.Name = "dateTimePickerBirthDate";
+			this.dateTimePickerBirthDate.Size = new System.Drawing.Size(216, 22);
+			this.dateTimePickerBirthDate.TabIndex = 39;
+			// 
+			// textBoxMiddleName
+			// 
+			this.textBoxMiddleName.Location = new System.Drawing.Point(177, 113);
+			this.textBoxMiddleName.Name = "textBoxMiddleName";
+			this.textBoxMiddleName.Size = new System.Drawing.Size(216, 22);
+			this.textBoxMiddleName.TabIndex = 38;
+			// 
+			// textBoxFirstName
+			// 
+			this.textBoxFirstName.Location = new System.Drawing.Point(177, 69);
+			this.textBoxFirstName.Name = "textBoxFirstName";
+			this.textBoxFirstName.Size = new System.Drawing.Size(216, 22);
+			this.textBoxFirstName.TabIndex = 37;
+			// 
+			// textBoxLastName
+			// 
+			this.textBoxLastName.Location = new System.Drawing.Point(177, 25);
+			this.textBoxLastName.Name = "textBoxLastName";
+			this.textBoxLastName.Size = new System.Drawing.Size(216, 22);
+			this.textBoxLastName.TabIndex = 36;
+			// 
+			// labelPhone
+			// 
+			this.labelPhone.AutoSize = true;
+			this.labelPhone.Location = new System.Drawing.Point(94, 248);
+			this.labelPhone.Name = "labelPhone";
+			this.labelPhone.Size = new System.Drawing.Size(70, 16);
+			this.labelPhone.TabIndex = 35;
+			this.labelPhone.Text = "Телефон:";
+			// 
+			// labelEmail
+			// 
+			this.labelEmail.AutoSize = true;
+			this.labelEmail.Location = new System.Drawing.Point(120, 204);
+			this.labelEmail.Name = "labelEmail";
+			this.labelEmail.Size = new System.Drawing.Size(44, 16);
+			this.labelEmail.TabIndex = 34;
+			this.labelEmail.Text = "Email:";
+			// 
+			// labelBirthDate
+			// 
+			this.labelBirthDate.AutoSize = true;
+			this.labelBirthDate.Location = new System.Drawing.Point(55, 160);
+			this.labelBirthDate.Name = "labelBirthDate";
+			this.labelBirthDate.Size = new System.Drawing.Size(109, 16);
+			this.labelBirthDate.TabIndex = 33;
+			this.labelBirthDate.Text = "Дата рождения:";
+			// 
+			// labelMiddleName
+			// 
+			this.labelMiddleName.AutoSize = true;
+			this.labelMiddleName.Location = new System.Drawing.Point(91, 116);
+			this.labelMiddleName.Name = "labelMiddleName";
+			this.labelMiddleName.Size = new System.Drawing.Size(73, 16);
+			this.labelMiddleName.TabIndex = 32;
+			this.labelMiddleName.Text = "Отчество:";
+			// 
+			// labelFirstName
+			// 
+			this.labelFirstName.AutoSize = true;
+			this.labelFirstName.Location = new System.Drawing.Point(128, 72);
+			this.labelFirstName.Name = "labelFirstName";
+			this.labelFirstName.Size = new System.Drawing.Size(36, 16);
+			this.labelFirstName.TabIndex = 31;
+			this.labelFirstName.Text = "Имя:";
+			// 
+			// labelLastName
+			// 
+			this.labelLastName.AutoSize = true;
+			this.labelLastName.Location = new System.Drawing.Point(95, 28);
+			this.labelLastName.Name = "labelLastName";
+			this.labelLastName.Size = new System.Drawing.Size(69, 16);
+			this.labelLastName.TabIndex = 30;
+			this.labelLastName.Text = "Фамилия:";
+			// 
 			// StudentForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(736, 369);
+			this.Controls.Add(this.pictureBoxPhoto);
+			this.Controls.Add(this.textBoxPhone);
+			this.Controls.Add(this.textBoxEmail);
+			this.Controls.Add(this.dateTimePickerBirthDate);
+			this.Controls.Add(this.textBoxMiddleName);
+			this.Controls.Add(this.textBoxFirstName);
+			this.Controls.Add(this.textBoxLastName);
+			this.Controls.Add(this.labelPhone);
+			this.Controls.Add(this.labelEmail);
+			this.Controls.Add(this.labelBirthDate);
+			this.Controls.Add(this.labelMiddleName);
+			this.Controls.Add(this.labelFirstName);
+			this.Controls.Add(this.labelLastName);
 			this.Controls.Add(this.labelID);
 			this.Controls.Add(this.buttonCancel);
 			this.Controls.Add(this.buttonOK);
@@ -113,6 +247,7 @@
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Name = "StudentForm";
 			this.Text = "StudentForm";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxPhoto)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -125,5 +260,18 @@
 		private System.Windows.Forms.Button buttonOK;
 		private System.Windows.Forms.Button buttonCancel;
 		private System.Windows.Forms.Label labelID;
+		private System.Windows.Forms.PictureBox pictureBoxPhoto;
+		private System.Windows.Forms.TextBox textBoxPhone;
+		private System.Windows.Forms.TextBox textBoxEmail;
+		private System.Windows.Forms.DateTimePicker dateTimePickerBirthDate;
+		private System.Windows.Forms.TextBox textBoxMiddleName;
+		private System.Windows.Forms.TextBox textBoxFirstName;
+		private System.Windows.Forms.TextBox textBoxLastName;
+		private System.Windows.Forms.Label labelPhone;
+		private System.Windows.Forms.Label labelEmail;
+		private System.Windows.Forms.Label labelBirthDate;
+		private System.Windows.Forms.Label labelMiddleName;
+		private System.Windows.Forms.Label labelFirstName;
+		private System.Windows.Forms.Label labelLastName;
 	}
 }
