7772f66212087eb66fc3412a79c328bddf1a998a
Veronika050987 <veronicaviv@mail.ru>
2025-10-13 20:52:40 +0300

    LINQ started

---
+++ LINQ/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			AllocConsole();
+
+			//1) Создаём источник данных
+			int[] arr = { 3, 5, 8, 13, 21 };
+
+			//2) Определяем query expression (возвращает коллекцию IEnumerable)
+			IEnumerable<int> FibonacciQuery =
+				from i in arr
+				where i > 10
+				orderby i descending
+				select i;
+
+			//3) Выполнение запроса
+			foreach(int i in FibonacciQuery)
+			{
+				Console.WriteLine($"{i}\t");
+			}
+			Console.WriteLine();
+		}
+		[DllImport("kernel32.dll")]
+		public static extern bool AllocConsole();
+		
+		[DllImport("kernel32.dll")]
+		public static extern bool FreeConsole();
+	}
+}
